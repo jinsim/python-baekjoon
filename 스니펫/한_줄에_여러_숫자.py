f7a0178c7b04b79b487a72e98fdee87d791ccf18
jinsim <simsime@korea.ac.kr>
2022-04-02 12:36:24 +0900

    스니펫 수정

--- 스니펫/한_줄에_여러_숫자.py
+++ 스니펫/한_줄에_여러_숫자.py
@@ -1,6 +1,10 @@
 # input이 "1, 2, 3, 4" 로 주어질 때.
 
-# 리스트로 변환
+# 여러 요소로 받기
+a, b, c, d = map(int, input().split())
+# a=1, b=2, c=3, d=4로 저장됨.
+
+# 리스트로 받기
 list(map(int, input().split()))  
 # [1, 2, 3, 4]
 
