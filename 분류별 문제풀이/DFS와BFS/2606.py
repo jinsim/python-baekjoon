8c283e5d0183edf57537f675fd697d2467ae6d17
jinsim <simsime@korea.ac.kr>
2022-07-03 14:43:18 +0900

    [2606] 바이러스

---
+++ 분류별 문제풀이/DFS와BFS/2606.py
@@ -0,0 +1,28 @@
+# 바이러스
+# 이어진 그래프 찾기 문제
+
+"""
+숫자형태로 변형해서 그래프를 생성하고, 방문한 요소는 True로 두어 연결된 노드 중 방문하지 않은 노드에 대해서만 dfs를 재귀 호출한다. 호출한 수가 연결된 노드의 수이다.
+"""
+from collections import defaultdict
+maze = defaultdict(list)
+n = int(input())
+m = int(input())
+for _ in range(m):
+    a, b = map(int, input().split())
+    maze[a].append(b)
+    maze[b].append(a)
+
+visited = [False]*(n+1)
+cnt = 0
+
+def dfs(i):
+    global cnt
+    visited[i] = True
+    for j in maze[i]:
+        if not visited[j]:
+            dfs(j)
+            cnt += 1
+        
+dfs(1)
+print(cnt)
