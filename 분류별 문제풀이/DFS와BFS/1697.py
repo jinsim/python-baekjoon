bfbb22a2ec190bcad247d08856f8d45fc252ff74
jinsim <simsime@korea.ac.kr>
2022-07-05 21:16:40 +0900

    [1697] 숨바꼭질

---
+++ 분류별 문제풀이/DFS와BFS/1697.py
@@ -0,0 +1,21 @@
+# 숨바꼭질
+# BFS의 기본적인 활용을 보는 문제
+
+"""
+핵심은 그래프에 cnt를 더해가며 저장하는 것이다. 
+"""
+from collections import deque
+n, k = map(int, input().split())
+graph = [0] * 100001
+graph[n] = 1
+Q = deque([n])
+
+while Q:
+    v = Q.popleft()
+    if v == k:
+        print(graph[v]-1)
+        break
+    for dv in [v-1, v+1, v*2]:
+        if 0 <= dv < 100001 and graph[dv] == 0:
+            graph[dv] = graph[v] + 1
+            Q.append(dv)
