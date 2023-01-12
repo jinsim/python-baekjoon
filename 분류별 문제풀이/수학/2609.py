f6c2ea32307b245be3a4dfa9cfc7fda6c0648e04
jinsim <simsime@korea.ac.kr>
2023-01-12 23:27:21 +0900

    [2609] : 최대공약수와 최소공배수

---
+++ 분류별 문제풀이/수학/2609.py
@@ -0,0 +1,15 @@
+"""
+유클리드 호제법을 통해 최대 공약수랑 최소 공배수 출력
+"""
+a, b = map(int, input().split())
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(gcd(a, b))
+print(lcm(a, b))
